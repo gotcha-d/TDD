e6dcc872a3efab800ba9be75a15631fcae65154a
gotcha-d <gotcha.privatemail@gmail.com>
2023-06-30 21:45:26 +0900

    第1章完了

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -2,4 +2,14 @@ package money;
 
 class Dollar {
 
+  int amount = 10;
+
+  Dollar(int amount) {
+    this.amount = amount;
+  }
+
+  void times(int multiplier) {
+    amount *= multiplier;
+  }
+  
 }
--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -6,5 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyTest {
     @Test
     public void testMultiplication() {
+      Dollar five = new Dollar(5);
+      five.times(2);
+      assertEquals(10, five.amount);
     }
 }
